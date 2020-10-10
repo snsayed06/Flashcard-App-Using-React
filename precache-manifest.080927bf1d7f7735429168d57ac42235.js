e1db58bd7a6e81f92c422d0f36309b93aeadc273
snsayed06 <snsayed06@gmail.com>
2020-10-09 17:45:28 -0700

    Updates

---
+++ precache-manifest.080927bf1d7f7735429168d57ac42235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef242b0014e49de609b7501fd47da5c6",
+    "url": "/Flashcard-App-Using-React/index.html"
+  },
+  {
+    "revision": "4cef97c59213d5a9003c",
+    "url": "/Flashcard-App-Using-React/static/css/main.bd385a52.chunk.css"
+  },
+  {
+    "revision": "2e45df308e79eff9aa5c",
+    "url": "/Flashcard-App-Using-React/static/js/2.29583c2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Flashcard-App-Using-React/static/js/2.29583c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cef97c59213d5a9003c",
+    "url": "/Flashcard-App-Using-React/static/js/main.51ad2b44.chunk.js"
+  },
+  {
+    "revision": "7a51bb07a2dc98ca8d3e",
+    "url": "/Flashcard-App-Using-React/static/js/runtime-main.674a2011.js"
+  }
+]);
